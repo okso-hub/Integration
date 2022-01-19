a4161e9ba073a637a0a5df9ab17bbc060839fb5b
Okan <okso2004@gmail.com>
2022-01-19 22:42:24 +0100

    Calculating integrals

--- main.py
+++ main.py
@@ -1,5 +1,29 @@
+from scipy.integrate import quad
+from os import system, name
+
+
+clear = lambda: system("clear") if name == "posix" else system("cls")
+
+
+borders = [int(input("Enter lower border: ")), int(input("Enter upper border: "))]
+
+a = float(input("Factor of x^5 = "))
+b = float(input("Factor of x^4 = "))
+c = float(input("Factor of x^3 = "))
+d = float(input("Factor of x^2 = "))
+e = float(input("Factor of x^1 = "))
+f = float(input("Factor of x^0 = "))
+
+
+def function(x):
+    return a*x**5 + b*x**4 + c*x**3 + d*x**2 + e*x + f
+
+
+i = quad(function, borders[0], borders[1])
+
+
 def main():
-    pass
+    print(i[0])
 
 
 if __name__ == "__main__":
